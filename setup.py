44850ef2423473a5ac0a5a8c63185cd0509c5560
Brian D. Weitzner <bweitzner@lyellbio.com>
2019-02-06 22:33:44 -0800

    bumping version

--- setup.py
+++ setup.py
@@ -5,50 +5,46 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = ['Click>=6.0', ]
+requirements = ["Click>=6.0"]
 
-setup_requirements = [ ]
+setup_requirements = []
 
-test_requirements = [ ]
+test_requirements = []
 
 setup(
     author="Brian D. Weitzner",
-    author_email='bweitzner@lyellbio.com',
+    author_email="bweitzner@lyellbio.com",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
         "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
     description="Amino acid reverse translation and DNA optimization tool based on species-specific codon-use distributions.",
-    entry_points={
-        'console_scripts': [
-            'codon_harmony=codon_harmony.cli:main',
-        ],
-    },
+    entry_points={"console_scripts": ["codon_harmony=codon_harmony.cli:main"]},
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     include_package_data=True,
-    keywords='codon_harmony',
-    name='codon_harmony',
-    packages=find_packages(include=['codon_harmony']),
+    keywords="codon_harmony",
+    name="codon_harmony",
+    packages=find_packages(include=["codon_harmony"]),
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    version='0.9',
+    version="0.9.2",
     zip_safe=False,
 )

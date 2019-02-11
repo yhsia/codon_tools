7f6c12f7f919f5e2dc05fcff43c81824f97dcd87
Brian D. Weitzner <bweitzner@lyellbio.com>
2019-02-11 13:19:46 -0800

    Bump version: 0.9.3-dev0 → 0.9.3

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     setup_requires=setup_requirements,
     test_suite="tests",
     tests_require=test_requirements,
-    version="0.9.3-dev0",
+    version="0.9.3",
     zip_safe=False,
 )

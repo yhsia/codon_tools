d3a9865378387af7e3892d0f2d3d85447d15033a
Brian D. Weitzner <bweitzner@lyellbio.com>
2019-02-14 14:02:14 -0800

    more codon_use tests

--- tests/test_codon_harmony_util.py
+++ tests/test_codon_harmony_util.py
@@ -21,6 +21,7 @@ class TestCodon_tools_data(unittest.TestCase):
         self.test_dna = Seq(
             "ACCGAGTCTACCACCGAGTCTACCACCGAGTCTACC", IUPAC.unambiguous_dna
         )
+        self.known_counts = {"ACC": 6, "GAG": 3, "TCT": 3}
 
     def tearDown(self):
         """Tear down test fixtures, if any."""
@@ -30,10 +31,20 @@ class TestCodon_tools_data(unittest.TestCase):
         assert self.test_aa.back_translate() == self.test_dna
 
     def test_004_codon_count(self):
-        known_counts = {"ACC": 6, "GAG": 3, "TCT": 3}
+        """Test `codon_harmony.util.codon_use.codon_count`"""
+
         cdn_count = chu.codon_use.count_codons(self.test_dna)
         for codon, count in cdn_count.items():
-            if codon in known_counts:
-                assert known_counts[codon] == count
+            if codon in self.known_counts:
+                assert self.known_counts[codon] == count
             else:
                 assert not count
+
+    def test_005_calc_profile(self):
+        """Test `codon_harmony.util.codon_use.calc_profile`"""
+        profile = chu.codon_use.calc_profile(chu.codon_use.count_codons(self.test_dna))
+        for codon, frequency in profile.items():
+            if codon in self.known_counts:
+                assert frequency == 1.0 and type(frequency) == float
+            else:
+                assert frequency == 0

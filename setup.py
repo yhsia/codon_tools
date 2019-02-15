969cb49b77759c512daaf37aef7235f5fa3b8c37
Brian D. Weitzner <bweitzner@lyellbio.com>
2019-02-15 13:40:22 -0800

    Adjustments to make package actually install via pip

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.rst") as readme_file:
 with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = ["Click>=6.0"]
+requirements = ["biopython>=1.73", "python_codon_tables>=0.1.6"]
 
 setup_requirements = []
 
@@ -25,11 +25,7 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
     ],
@@ -41,7 +37,7 @@ setup(
     include_package_data=True,
     keywords="codon_harmony",
     name="codon_harmony",
-    packages=find_packages(include=["codon_harmony"]),
+    packages=find_packages(exclude=["tests"]),
     setup_requires=setup_requirements,
     test_suite="tests",
     tests_require=test_requirements,

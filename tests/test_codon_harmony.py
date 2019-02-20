12ac2ce4c818a79cdb11c45888335ef00a0d512a
Brian D. Weitzner <bweitzner@lyellbio.com>
2019-02-19 16:49:58 -0800

    Adding a test for arg parsing -- will fail if cli or defaults are changed

--- tests/test_codon_harmony.py
+++ tests/test_codon_harmony.py
@@ -18,5 +18,28 @@ class TestCodon_tools(unittest.TestCase):
     def tearDown(self):
         """Tear down test fixtures, if any."""
 
-    def test_000_something(self):
-        """Test something."""
+    def test_args(self):
+        """Test args parsing."""
+        args_to_parse = ["--input", "dummy.fasta", "--output", "out.fasta"]
+        parser = codon_harmony.get_parser()
+        parsed_args = parser.parse_args(args_to_parse)
+        print()
+        print(parsed_args)
+        assert parsed_args.input == "dummy.fasta"
+        assert parsed_args.output == "out.fasta"
+        assert parsed_args.cycles == 100
+        assert parsed_args.host == "413997"
+        assert parsed_args.host_threshold == 0.1
+        assert parsed_args.inner_cycles == 10
+        assert parsed_args.local_homopolymer_threshold == 4
+        assert parsed_args.max_relax == 0.1
+        assert parsed_args.restriction_enzymes == [
+            "NdeI",
+            "XhoI",
+            "HpaI",
+            "PstI",
+            "EcoRV",
+            "NcoI",
+            "BamHI",
+        ]
+        assert parsed_args.verbose == 0
